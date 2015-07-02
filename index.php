94b730b4b9d44dc4de45f9a6c93221aa2bd8200b
Ariana <dorado_ariana@yahoo.com>
2015-07-02 14:13:50 -0600

    Added PHP to title and year. Changed file to PHP.

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
 <head>
-	<title>Shirts 4 Mike</title>
+	<title><?php echo "Shirts 4 Mike"; ?></title>
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Oswald:400,700" type="text/css">
 	<link rel="shortcut icon" href="favicon.ico">
@@ -84,7 +84,7 @@
 				<li><a href="https://www.facebook.com/TeamTreehouse">Facebook</a></li>
 			</ul>
 
-			<p>&copy;2012 Shirts 4 Mike</p>
+			<p>&copy;<?php echo date('Y'); ?> Shirts 4 Mike</p>
 
 		</div>
 	
